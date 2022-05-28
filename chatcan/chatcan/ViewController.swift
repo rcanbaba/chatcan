0400760088c34a5cd2d84fac3b2106230c0d864d
rcanbaba <rcanbabaoglu@gmail.com>
2022-05-28 13:24:13 +0300

    -project created, initial settings completed.

---
+++ chatcan/chatcan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatcan
+//
+//  Created by Can Babaoğlu on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
